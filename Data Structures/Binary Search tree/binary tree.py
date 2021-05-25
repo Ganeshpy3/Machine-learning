200791c4ea692186de4f98c50c6b940e6953985e
Ganesh <59509677+Ganeshpy3@users.noreply.github.com>
2021-05-25 13:28:11 +0530

    Update binary tree.py

--- Data Structures/Binary Search tree/binary tree.py
+++ Data Structures/Binary Search tree/binary tree.py
@@ -17,25 +17,73 @@ class binary_tree:
             else:
                 self.left=binary_tree(data)
 
-    def inorder(self,li=[]):
+    def inorder(self):
+        li=[]
 
         if self.left:
-            self.left.inorder()
+            li+=self.left.inorder()
 
         li.append(self.data)
 
         if self.right:
-            self.right.inorder()
+            li+=self.right.inorder()
         return  li
-    def preorder(self,li=[]):
+    def preorder(self):
+        li=[]
         li.append(self.data)
         if self.left:
-            self.left.preorder()
+            li+=self.left.preorder()
 
         if self.right:
-            self.right.preorder()
+            li+=self.right.preorder()
 
         return li
+
+    def postorder(self):
+        li = []
+        if self.left:
+            li+=self.left.postorder()
+        if self.right:
+            li+=self.right.postorder()
+
+        li.append(self.data)
+
+        return li
+
+    def max(self):
+        if self.right is None:
+            return self.data
+        else:
+            return self.right.max()
+
+    def min(self):
+        if self.left is None:
+            return self.data
+        else:
+            return self.left.min()
+
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            min_val = self.right.min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
 
 
 a=binary_tree(100)
@@ -47,3 +95,12 @@ def add_values(data):
 add_values(data)
 print(a.inorder())
 print(a.preorder())
+print(a.postorder())
+print(a.max())
+print(a.min())
+print(a.left.right.left.min())
+print(a.inorder())
+a.delete(10)
+
+
+print(a.inorder())
